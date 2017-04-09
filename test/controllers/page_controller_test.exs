4b4317f696f7a254db76e3dd18cc8f7ed9bc085b
Paul Götze <paul.christoph.goetze@gmail.com>
2017-04-09 15:02:22 +0200

    Fix tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CrowdScored.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to CrowdScored"
   end
 end
